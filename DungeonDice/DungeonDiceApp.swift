1ef2a59c1bb0194c20aab674c2ba3cbf006bd11c
Philip Keller <philipjoelkeller@gmail.com>
2023-02-16 13:18:23 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Philip Keller on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
